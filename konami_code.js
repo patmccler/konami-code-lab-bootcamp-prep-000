3262ecd346be0d5f295ae549aa067a116dd7b2f0
Patrick McClernan <patmccler@gmail.com>
2017-10-27 18:03:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,6 +4,23 @@ function init() {
 
   var body = document.querySelector('body')
 
+  body.addEventListener('keydown', onKeyDownHandler(e) )
 
 
+}
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
