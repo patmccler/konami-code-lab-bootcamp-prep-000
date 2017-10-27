0a4e3654566c8a4b62b67b74f24e9c6a884afefe
Patrick McClernan <patmccler@gmail.com>
2017-10-27 18:03:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,13 +11,13 @@ function init() {
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
