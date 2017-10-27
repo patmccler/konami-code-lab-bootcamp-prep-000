5b4a9306fdb658a51407fc3912d3b51e61b8751b
Patrick McClernan <patmccler@gmail.com>
2017-10-27 18:06:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  var body = document.querySelector('body')
+  let body = document.querySelector('body')
 
   body.addEventListener('keydown', onKeyDownHandler(event) )
 
